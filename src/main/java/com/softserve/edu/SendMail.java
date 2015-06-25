469eda1bf73882960670fea6b32f95e21237c103
NazarLushey <nlushney@gmail.com>
2015-06-25 09:24:55 +0300

    New changes

---
+++ src/main/java/com/softserve/edu/SendMail.java
@@ -0,0 +1,61 @@
+package com.softserve.edu;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.testng.log4testng.Logger;
+
+public class SendMail {
+
+	
+	private final static Logger helperLogger = Logger.getLogger(SendMail.class);
+
+	public static void SendMailToInbox(String formatedWeather) {
+     		
+        final String username = "nlushney@gmail.com";
+        final String password = "capturetheflag";
+        final String recipient = "nlushney@gmail.com";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+     
+ //       Helper helper = new Helper(); 
+        
+        Session session = Session.getInstance(props,
+          new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+          });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
+            message.setSubject("Weather");
+            message.setContent(formatedWeather, "text/html; charset=utf-8");
+            
+            Transport.send(message);
+
+//            System.out.println("Done");
+            helperLogger.info("Done");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
---
+++ src/test/java/SendOnMail.java
@@ -0,0 +1,43 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.log4testng.Logger;
+
+import com.softserve.edu.Helper;
+import com.softserve.edu.SendMail;
+
+
+/**
+ * Created by Xdr on 6/4/15.
+ */
+
+public class SendOnMail {
+    private WebDriver driver;
+    private Helper helper;
+    private final static Logger helperLogger = Logger.getLogger(SendMail.class);
+ 
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new FirefoxDriver();
+        helperLogger.info("Browser start");
+        
+        helper = new Helper(driver);
+    }
+
+    @Test
+    public void send() {
+    	driver.get("http://weather.i.ua/");
+    	SendMail.SendMailToInbox(helper.getFormatedWeather());
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
